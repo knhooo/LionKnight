0f664241de85dcc70041a7fe81fdbd209c1a446e
chlghksgml01 <58538862+chlghksgml01@users.noreply.github.com>
2025-04-25 20:46:04 +0900

    [feat] Graimm 등장 전 카메라 이동

--- Assets/HwanHee/Scripts/GrimmMapCameraController.cs
+++ Assets/HwanHee/Scripts/GrimmMapCameraController.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using Unity.Cinemachine;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.Splines.Interpolators;
 
 public class GrimmMapCameraController : MonoBehaviour
 {
     [SerializeField] private Collider2D bossStartTrigger;
-    [SerializeField] private CinemachineConfiner2D confiner;
+    [SerializeField] private CinemachineConfiner2D cameraConfiner;
     [SerializeField] private BoxCollider2D bossBoundingShape;
-    [SerializeField] private Vector2 CameraPos1;
-    [SerializeField] private Vector2 CameraPos2;
-    [SerializeField] private Vector2 CameraPos3;
+    [SerializeField] private Transform[] CameraPos;
+    [SerializeField] private float cameraMoveTime;
     [SerializeField] private FadeSprite fadeSprite;
 
     Player player;
@@ -27,16 +28,36 @@ public class GrimmMapCameraController : MonoBehaviour
     {
         if (collision.transform == bossStartTrigger.transform)
         {
-            CameraMove();
+            transform.SetParent(null);
+            StartCoroutine(CameraMove());
             StartCoroutine(fadeSprite.StartFadeOut());
         }
     }
 
-
-
-    private void CameraMove()
+    private IEnumerator CameraMove()
     {
+        int i = 0;
+        while (true)
+        {
+            float elapsed = 0f;
+            Vector3 currentPos = CameraPos[i].transform.position;
+
+            while (elapsed < cameraMoveTime)
+            {
+                transform.position = Vector3.Lerp(currentPos, CameraPos[i + 1].transform.position, elapsed / cameraMoveTime);
+
+                elapsed += Time.deltaTime;
+
+                yield return null;
+            }
+            currentPos = CameraPos[i].transform.position;
+
+            i++;
+
+            if (i == CameraPos.Length - 1)
+                break;
+        }
     }
 
-    private void SwitchConfiner() => confiner.BoundingShape2D = bossBoundingShape;
+    private void SwitchConfiner() => cameraConfiner.BoundingShape2D = bossBoundingShape;
 }
