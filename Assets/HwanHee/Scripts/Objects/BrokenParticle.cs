c49b673417566f5e679f825e30659e38ee4c2c72
chlghksgml01 <58538862+chlghksgml01@users.noreply.github.com>
2025-05-07 00:14:50 +0900

    [design] Stag Station Light, Particle

--- Assets/HwanHee/Scripts/Objects/BrokenParticle.cs
+++ Assets/HwanHee/Scripts/Objects/BrokenParticle.cs
@@ -5,11 +5,23 @@ public class BrokenParticle : AddForceObject
     [SerializeField] private Sprite[] sprites;
     private SpriteRenderer sr;
 
+    private bool justInstantiated = true;
+
     protected override void Awake()
     {
         base.Awake();
         sr = GetComponent<SpriteRenderer>();
         sr.sprite = sprites[Random.Range(0, 4)];
+    }
+
+    protected override void OnEnable()
+    {
+        if (justInstantiated)
+        {
+            justInstantiated = false;
+            return;
+        }
+        base.OnEnable();
     }
 
     public void SetSortingInfo(int sortingLayerID, int sortingOrder)
