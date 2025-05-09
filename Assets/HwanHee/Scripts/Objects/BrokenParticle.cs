fc5ac735a3cd28e70ff4100d907f69764a933301
chlghksgml01 <chlghksgml01@naver.com>
2025-05-09 22:03:13 +0900

    [chore] BrokenParticle 위치 조절

--- Assets/HwanHee/Scripts/Objects/BrokenParticle.cs
+++ Assets/HwanHee/Scripts/Objects/BrokenParticle.cs
@@ -10,6 +10,10 @@ public class BrokenParticle : MonoBehaviour
         sr = GetComponent<SpriteRenderer>();
         sr.sprite = sprites[Random.Range(0, 4)];
     }
+    private void OnEnable()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + 1f, 0);
+    }
 
     public void SetSortingInfo(int sortingLayerID, int sortingOrder)
     {
