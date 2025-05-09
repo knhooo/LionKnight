080cea976c13c8d8312d608926127068383be45d
chlghksgml01 <chlghksgml01@naver.com>
2025-05-09 20:44:54 +0900

    [fix] 다른 씬에서 시작하면 오류생기는거 수정

--- Assets/HwanHee/Scripts/Manager/DataManager.cs
+++ Assets/HwanHee/Scripts/Manager/DataManager.cs
@@ -18,6 +18,7 @@ public class DataManager : Singleton<DataManager>
     protected override void Awake()
     {
         base.Awake();
+        path = Path.Combine(Application.dataPath, "..", "Saves", saveFiles);
     }
 
     public void PrepareSaveDirectory()
