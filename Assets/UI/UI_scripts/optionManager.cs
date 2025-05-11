e7520a20cf0d12c435de57993048f94be009584d
UH <85742380+KTW1@users.noreply.github.com>
2025-05-12 04:28:56 +0900

    UI_commitlast

--- Assets/UI/UI_scripts/optionManager.cs
+++ Assets/UI/UI_scripts/optionManager.cs
@@ -38,15 +38,19 @@ public class optionManage : MonoBehaviour
     //public Button[] buttons;
     //private GameObject curButton;
 
-    void Awake()
+    private void Awake()
     {
-        BossDeadCheck();
         BttAnimator();
 
         credit.loopPointReached += OnCreditVideoEnd;
         credit.isLooping = false;
 
         SetMainUI();
+    }
+
+    private void Start()
+    {
+        BossDeadCheck();
     }
 
     private void BttAnimator()
@@ -71,7 +75,7 @@ public class optionManage : MonoBehaviour
 
     private void BossDeadCheck()
     {
-        if (DataManager.instance.bossDeadData.isDead)
+        if (DataManager.instance.bossDeadData.isDead == true)
         {
             ActivateObjects();
         }
